89e0481c12a82a14fe8811c6089789ba4598699f
John Ward <ward.johnm@gmail.com>
2015-07-08 16:26:35 -0700

    fixes uninited variable bug in statics controller

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,8 +1,10 @@
 class StaticsController < ApplicationController
 
 	def index
-		remspace = params[:search_term].downcase.tr!(" ", "_")
-		@beer = Beer.search(remspace)
+		if params[:search_term].present?
+			remspace = params[:search_term].downcase.tr!(" ", "_")
+			@beer = Beer.search(remspace)
+		end
 
 	end
 
