c9f511ad32520a8e8ed2e4bb524a4a200fcd2b53
Samuel Amiri <samuelamiri@gmail.com>
2015-07-08 15:54:17 -0700

    updated google maps with seed data

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,10 @@
 class StaticsController < ApplicationController
 
 	def index
+		@restaurants = Restaurant.all 
+		@hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+		  marker.lat restaurant.lat
+		  marker.lng restaurant.long
+		end
 	end
 end
