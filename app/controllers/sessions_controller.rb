1b5648c9be6310f7cf2c8b6dda730c27281decee
Busyra Anwar <busyra@me.com>
2015-07-07 20:27:24 -0700

    revises create function in sessions controller to confirm user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
 	def create
-    	render text: request.env['omniauth.auth'].to_yaml
-  	end
+		begin
+			@user = User.from_omniauth(request.env['omniauth.auth'])
+			session[:user_id] = @user.id
+			flash[:success] = "Welcome, #{@user.name}!"
+		rescue
+			flash[:warning] = "There was an error while trying to authenticate you..."
+		end
+		redirect_to root_path
+	end
 end
