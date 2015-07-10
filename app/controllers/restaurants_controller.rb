9c1357e2e2f71badda284e24a909a90b98ec5d2c
Samuel Amiri <samuelamiri@gmail.com>
2015-07-09 17:35:09 -0700

    Set new restaurant route to go to id

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,7 +21,7 @@ class RestaurantsController < ApplicationController
   def create
       @restaurant = Restaurant.new(restaurant_params)
           if @restaurant.save
-              redirect_to restaurant_path
+              redirect_to restaurant_path(@restaurant.id)
           else
               render :index
           end
