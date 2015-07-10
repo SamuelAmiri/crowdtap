ab0e814858e66453c6bd7199a925a7035b55960d
John Ward <ward.johnm@gmail.com>
2015-07-09 17:32:33 -0700

    added form for restaurant creation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,12 +2,17 @@ class RestaurantsController < ApplicationController
 
 
   def index
+
     if params[:search_term].present?
       @client = GooglePlaces::Client.new(ENV["googleplaces_api_key"])
       searchTerm = params[:search_term]
       query =  searchTerm + " near Santa Monica"
-      @restaurant = @client.spots_by_query( query, :types => ['restaurant', 'bar'])
+      @place = @client.spots_by_query( query, :types => ['restaurant', 'bar'])
     end
   end
 
+private
+	def restaurant_params
+		params.permit(:name, :lat, :long)
+	end
 end
