8e42365f895ffd4bd7b5e1d8852ae8cd5b256d61
John Ward <ward.johnm@gmail.com>
2015-07-13 20:10:29 -0700

    adds ng-repeat and working nghide/show for beer search

--- angular-app/js/main.js
+++ angular-app/js/main.js
@@ -1,17 +1,25 @@
 angular
 	.module('beerApp', [])
 	.controller('BeerController', BeerController);
-	function BeerController($http) {
+
+	function BeerController($http,$scope) {
       var self = this;
       self.search_term = false;
+      self.beers = [0,1,2,3]
+      
       self.queryBrewDB = function(){
       	self.search_term = true;
       	url = "http://api.brewerydb.com/v2/search?q="+self.beerparam+"&type=beer&key=3fa253bbc1552ae76cdad8987cd4386b"
       	$.get(url, function(results){
       		self.beers = results["data"];
       		console.log(self.beers)
+      		$scope.$apply()
       	})
       }
-  }
-  
-      
+      self.selectBeer = function(index){
+      	self.beerID = self.beers[index].id
+      	self.search_term = false;
+      }
+    }
+ 
+  	
